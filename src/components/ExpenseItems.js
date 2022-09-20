4a0e04060447f593762b90eedbf1be2dec08490d
tiwarinitin <tnit66@gmail.com>
2022-09-21 00:29:42 +0530

    file chnagd

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,17 +2,7 @@ import "../styles/ExpenseItems.css";
 
 function ExpenseItems(props) {
   return (
-    <div className="main">
-      <div className="item">
-        <div className="item-date">
-          <p>{props.date.getFullYear()}</p>
-          <p>{props.date.toLocaleString('en-US', {month: 'long'})}</p>
-          <p>{props.date.getDate()}</p>
-        </div>
-        <div className="item-title">{props.title}</div>
-        <div className="item-price">{props.price}</div>
-      </div>
-    </div>
+    <p>hello world!!</p>
   );
 }
 
